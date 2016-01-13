d3409a236a9752f0d2b9d6ac1a5bd5d8d6992282
최아연 <zinc@choeayeon-ui-MacBook-Pro.local>
2016-01-13 19:28:41 +0900

    add play-service-ads

--- Capstone/app/src/main/java/com/example/zinc/capstone/AboutActivity.java
+++ Capstone/app/src/main/java/com/example/zinc/capstone/AboutActivity.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class AboutActivity extends AppCompatActivity {
+    private InterstitialAd mInterstitialAd;
+    private Button mADButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,63 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mADButton = ((Button) findViewById(R.id.ad_button));
+        mADButton.setEnabled(false);
+        mADButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInterstitial();
+            }
+        });
+
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
     }
 
+
+    private InterstitialAd newInterstitialAd() {
+        InterstitialAd interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                mADButton.setEnabled(true);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                mADButton.setEnabled(true);
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Proceed to the next level.
+                prepareAD();
+            }
+        });
+        return interstitialAd;
+    }
+
+
+    private void showInterstitial() {
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+            prepareAD();
+        }
+    }
+
+    private void prepareAD() {
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
+    }
+    private void loadInterstitial() {
+        // Disable the next level button and load the ad.
+        mADButton.setEnabled(false);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        mInterstitialAd.loadAd(adRequest);
+    }
 }
